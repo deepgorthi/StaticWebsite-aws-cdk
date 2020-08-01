b9ac9c89660bd8c3ad0f9254f3c658af55b827f5
Deep Gorthi <deep.gorthi@gmail.com>
2020-08-01 17:12:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@
 from aws_cdk import core
 
 from static_website_aws_cdk.static_website_aws_cdk_stack import StaticWebsiteAwsCdkStack
-
+from static_website_aws_cdk.aws_certificate_manager_stack import AWSCertificateManagerStack
 
 app = core.App()
-StaticWebsiteAwsCdkStack(app, "static-website-aws-cdk")
+AWSCertificateManagerStack(app, "static-website-cert", env=core.Environment(region="us-east-1"))
+StaticWebsiteAwsCdkStack(app, "static-website-aws-cdk", env=core.Environment(region="us-east-1"))
 
 app.synth()
